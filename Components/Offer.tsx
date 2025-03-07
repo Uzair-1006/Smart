e91e484289033d6f980792bc737329b610d3a4c4
Shaik Uzair Ahmed <shaikuzair57@gmail.com>
2025-03-07 16:02:23 +0530

    Type Error

--- Components/Offer.tsx
+++ Components/Offer.tsx
@@ -12,12 +12,13 @@ const Offer = () => {
     return () => clearInterval(timer);
   }, []);
 
-  const formatTime = (seconds) => {
+  const formatTime = (seconds: number) => {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
   };
+  
 
   return (
     <section 
